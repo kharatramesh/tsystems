8b68ae51e09297e584e4b674df0ea7c560d2c4ac
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2026-09-22 19:44:43 +0530

    Add files via upload

--- day2/Assignments-sol/sg.tf
+++ day2/Assignments-sol/sg.tf
@@ -2,29 +2,15 @@ resource "aws_security_group" "sg1" {
   name        = var.sgname
   description = "Allow SSH and HTTP"
   vpc_id      = aws_vpc.vpc1.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
-  }
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-}
+  dynamic "ingress" {
+    for_each = var.sgports
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.description
+    }
 
+  }
 }
--- day2/Assignments-sol/var.tf
+++ day2/Assignments-sol/var.tf
@@ -4,3 +4,5 @@ variable "akey" { type = string }
 variable "skey" { type = string }
 variable "subnets" { type = map(object({ subnet_name = string, cidr_block = string, azone = string })) }
 variable "igw" { type = string }
+variable "sgname" { type = string }
+variable "sgports" { type = list(object({ from_port = number, to_port = number, description = string, protocol = string, cidr_blocks = list(string) })) }
