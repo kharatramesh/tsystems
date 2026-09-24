93f9ad3b16b8d92c8edc0036a396e770994a5f32
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2026-09-24 15:29:52 +0530

    Add files via upload

--- day3/module-Learning/compute/compute.tf
+++ day3/module-Learning/compute/compute.tf
@@ -15,13 +15,49 @@ data "aws_ami" "latest_ubuntu_24" {
   }
 }
 
+resource "aws_key_pair" "keypair1" {
+  key_name   = "trainer-ec2-key"
+  public_key = file("C:/Users/ADMIN/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "ec2" {
     ami = data.aws_ami.latest_ubuntu_24.id
     instance_type = var.itype
     subnet_id = var.subnet_id
     availability_zone = var.azone
-    # vpc_security_group_ids = var.sg
+    key_name = aws_key_pair.keypair1.key_name
+    security_groups = [ var.sgname ]
     tags = {
         Name = "Ec2-trainer"
     }
+    # provisioner "local-exec" {
+    #   command = "echo Learning Modules with Provisioners >> index.html"
+    # }
+
+    provisioner "file" {
+      source = "index.html"
+      destination = "/tmp/index.html"
+    }
+    provisioner "file" {
+      source = "web.sh"
+      destination = "/tmp/web.sh" 
+    }
+    provisioner "remote-exec" {
+      inline = [ 
+
+        "sudo setfacl -m u:ubuntu:rwx /var/www/html/",
+        "sudo setfacl -m u:ubuntu:rwx /tmp/",
+        "sudo chmod +x /tmp/web.sh",
+        "sudo /tmp/web.sh",
+        "sudo cp /tmp/index.html /var/www/html/"
+       ]
+    }
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = self.public_ip
+      timeout = "3m"
+      private_key = file("C:/Users/ADMIN/.ssh/id_rsa")
+    }
+
 }
--- day3/module-Learning/compute/variable.tf
+++ day3/module-Learning/compute/variable.tf
@@ -1,4 +1,4 @@
 variable "itype" { type=string }
 variable "subnet_id" {type=string}
 variable "azone" { type =string }
-# variable "sg" {type = list(string)}
+variable "sgname" {type = string}
